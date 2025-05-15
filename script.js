250dbf7007f9e22f76efcab2ea3ed1204fe429af
FabriAV <acostafabricio2007@gmail.com>
2025-05-15 00:06:11 -0500

    updated sections, improved desing

---
+++ script.js
@@ -0,0 +1,8 @@
+var mapita= L.map('map').setView([-6.785683, -79.839628], 17);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapita);
+L.control.scale().addTo(mapita);
+var marker=L.marker([-6.785683, -79.839628],{draggable: false}).addTo(mapita);
+marker.bindPopup("<b>¡VISÍTANOS!</b><br>te esperamos.").openPopup();
